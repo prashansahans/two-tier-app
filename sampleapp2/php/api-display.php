6bb7b15dcea7ae968fff7efd7997d7e212d29231
Prashansa <32064980+prashansahans@users.noreply.github.com>
2020-05-18 13:17:16 +0530

    Update api-display.php

--- sampleapp2/php/api-display.php
+++ sampleapp2/php/api-display.php
@@ -47,6 +47,7 @@ $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, 'https://vault-vaulttest4.router.default.svc.cluster.local/v1/auth/kubernetes/login');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"jwt\":\"$OCP_TOKEN\",\"role\":\"mysql-creds-roles\"}");
 
 $headers = array();
