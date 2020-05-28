7ebc209d474f2c80f2f52cd09cfb9295b7456e7f
Prashansa <32064980+prashansahans@users.noreply.github.com>
2020-05-28 14:54:06 +0530

    Update api-display.php

--- sampleapp2/php/api-display.php
+++ sampleapp2/php/api-display.php
@@ -20,7 +20,7 @@ $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, 'https://vault-vaulttest4.router.default.svc.cluster.local/v1/auth/token/create/apps-creds-role');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
 curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"policies\": \"apps-creds-policy\",\"meta\": {\"user\": \"test\"},\"ttl\": \"1h\",\"renewable\": true}");
 
 $headers = array();
