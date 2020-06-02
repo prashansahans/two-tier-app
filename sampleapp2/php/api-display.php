0d3498a97bb156b516fd5e5526f9e8d93dcbf983
Prashansa <32064980+prashansahans@users.noreply.github.com>
2020-06-02 21:04:56 +0530

    Update api-display.php

--- sampleapp2/php/api-display.php
+++ sampleapp2/php/api-display.php
@@ -134,18 +134,18 @@ if(isset($_GET['hello'])) {
 	runMyFunction();
 }
 function runMyFunction(){
+	echo "Hello";
 $ch = curl_init();
-
 curl_setopt($ch, CURLOPT_URL, 'https://vault-vaulttest4.router.default.svc.cluster.local/v1/auth/token/revoke-self');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 curl_setopt($ch, CURLOPT_POST, 1);
-
 $headers = array();
 $headers[] = 'X-Vault-Token:'.$client_token;
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
 $result = curl_exec($ch);
+echo $result;
 if (curl_errno($ch)) {
     echo 'Error:' . curl_error($ch);
 }
