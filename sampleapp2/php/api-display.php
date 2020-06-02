a15700f5f8f20d18208d2a0e1001a3b6a154603c
Prashansa <32064980+prashansahans@users.noreply.github.com>
2020-06-02 22:11:13 +0530

    Update api-display.php

--- sampleapp2/php/api-display.php
+++ sampleapp2/php/api-display.php
@@ -139,7 +139,7 @@ function runMyFunction(){
 	echo $client_token;
 $ch = curl_init();
 
-curl_setopt($ch, CURLOPT_URL, 'https://vault-vaulttest4.router.default.svc.cluster.local/v1/auth/token/revoke-self');
+curl_setopt($ch, CURLOPT_URL, 'https://vault-vaulttest4.router.default.svc.cluster.local/v1/auth/token/revoke');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 curl_setopt($ch, CURLOPT_POST, 1);
