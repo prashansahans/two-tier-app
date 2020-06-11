559889dfe48e2a8c4b18f1c4136e79fae58a4056
Prashansa <32064980+prashansahans@users.noreply.github.com>
2020-06-11 13:51:10 +0530

    Update api-display.php

--- sampleapp2/php/api-display.php
+++ sampleapp2/php/api-display.php
@@ -21,9 +21,9 @@ if (! empty($_SESSION['logged_in']))
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, 'https://vault-vaulttest4.router.default.svc.cluster.local/v1/auth/token/create/apps-creds-role');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,0);
-curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,1);
-curl_setopt($ch,CURLOPT_CAINFO, getcwd() . "/opt/app-root/src/sampleapp2/php/router-default-svc-cluster-local.pem");
+//curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,0);
+//curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,1);
+//curl_setopt($ch,CURLOPT_CAINFO, getcwd() . "/opt/app-root/src/sampleapp2/php/router-default-svc-cluster-local.pem");
 //curl_setopt($ch,CURLOPT_CAPATH,'check.crt');
 curl_setopt($ch, CURLOPT_POST, 1);
 
